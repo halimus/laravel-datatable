47dd4553d75cfbc73e4749f28a93615cf6467d49
W7 <W7@W7-PC>
2016-11-10 17:53:08 -0500

    add script_data.sql

---
+++ script_data.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2016 at 11:51 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `laravel-datatable`
+--
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Username1', 'lastname1@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(2, 'Sara', 'lastname2@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(3, 'Username3', 'lastname3@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(4, 'Username4', 'lastname4@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(5, 'Username5', 'lastname5@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(6, 'Sara', 'lastname6@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(7, 'Sara', 'Toualbi@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(8, 'Katia', 'toualbi.katia@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(9, 'Kahina', 'K.amel@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(10, 'Username8', 'lastname8@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(11, 'Halim', 'lardjane@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(12, 'Lardjane', 'halimo@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(13, 'Simpson', 'alberto@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(14, 'Rose', 'Denise@yahoo.com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(15, 'Milanya', 'Milanya@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(16, 'Alex', 'a.top@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(17, 'Alexxandra', 'Alexandra@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(18, 'Wassila', 'Tilmatine@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(19, 'Kafia', 'Tarakki@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(20, 'Nacim', 'hamiddi@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(21, 'sara', 'lebnadem@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(22, 'makan walo', 'kiwww@gmail,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(23, 'Sara', 's.lardjane@yahoo,com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(24, 'Kamila', 'karima@hotmail.com', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00'),
+(25, 'kamelia', 'kamelia.izro@yahoo.fr', 'aaaaaa', 'aaaaaaaaaa', '2016-11-03 04:00:00', '2016-11-08 05:00:00');
