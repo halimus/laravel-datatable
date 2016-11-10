3ad10eb543e25d24719f71e645ac2c5ce5fdd842
W7 <W7@W7-PC>
2016-11-10 02:35:55 -0500

    create layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{ config('app.name', 'Laravel-DataTable') }}</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="main">
+        
+            @include('layouts.navbar')
+
+            @yield('content')
+        
+        
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="{{ asset('js/script.js') }}" type="text/javascript"></script>
+        
+        @stack('scripts')
+        
+    </body>
+</html>
+
